e6992062324b12ff5771ef593e4f90af7089f3fd
Dario B <feddar@users.noreply.github.com>
2019-12-03 19:20:03 +0100

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,6 +2,11 @@
 
 export PID_FILE=/var/run/rbldnsd.pid
 
+ZONES=${ZONES:-/zones}
+USERNAME=${USERNAME:-rbldns}
+RBL_DOMAIN=${RBL_DOMAIN:-bl.localhost.tld}
+NS_SERVERS=${NS_SERVERS:-127.0.0.1}
+
 mkdir -p $ZONES
 chown $USERNAME $ZONES
 
@@ -18,7 +23,7 @@ su $USERNAME bash -c "
   [ -e $wl ] || touch $wl
   if [ ! -e forward ]; then
     echo '\$SOA' 3600 $RBL_DOMAIN $RBL_DOMAIN 0 600 300 86400 300 >forward
-    echo '\$NS' 3600 \$NS_SERVERS >>forward
+    echo '\$NS' 3600 $NS_SERVERS >>forward
   fi"
 
 rbldnsd -f -n -r $ZONES -b 0.0.0.0/53 -p $PID_FILE \
