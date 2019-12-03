11cb538dc5bee4c11b6165e7bd15a5a3f3c7535a
Dario B <feddar@users.noreply.github.com>
2019-12-03 19:30:57 +0100

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -17,14 +17,16 @@ bl="bl"
 # whitelist
 wl="wl"
 
-su $USERNAME bash -c "
-  cd $ZONES
-  [ -e $bl ] || touch $bl
-  [ -e $wl ] || touch $wl
-  if [ ! -e forward ]; then
-    echo '\$SOA' 3600 $RBL_DOMAIN $RBL_DOMAIN 0 600 300 86400 300 >forward
-    echo '\$NS' 3600 $NS_SERVERS >>forward
-  fi"
+
+cd $ZONES
+[ -e $bl ] || touch $bl
+[ -e $wl ] || touch $wl
+if [ ! -e forward ]; then
+  echo '$SOA' 3600 $RBL_DOMAIN $RBL_DOMAIN 0 600 300 86400 300 >forward
+  echo '$NS' 3600 $NS_SERVERS >>forward
+fi
+
+chown $USERNAME $bl $wl forward
 
 rbldnsd -f -n -r $ZONES -b 0.0.0.0/53 -p $PID_FILE \
   $RBL_DOMAIN:ip4set:$bl,$wl \
