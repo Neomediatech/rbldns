8a55398626e8a4e09a585d8c56d12de99410b052
Dario B <feddar@users.noreply.github.com>
2020-02-12 16:21:12 +0100

    more automation

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -36,24 +36,30 @@ if [ ! -e forward ]; then
   echo '$NS' 3600 $NS_SERVERS >>forward
 fi
 
+tmpzone=""
 if [ -n "$CUSTOM_ZONES" ]; then
   IFS=,
-  tmpzone=""
   echo "Configuring custom zones..."
   for zone in $CUSTOM_ZONES; do
-    if [ -s $zone ]; then
-      tmpzone="$tmpzone,$zone"
+    if [ -f $zone ]; then
+      #tmpzone="$tmpzone,$zone"
+      tmpzone="$tmpzone $RBL_DOMAIN:ip4set:$zone"
     else
-      echo "WARN: file $ZONES/$zone does't exists or has 0 size, skipping configuring it..."
+      echo "WARN: file $ZONES/$zone does't exists, skipping configuring it..."
     fi
   done
   unset IFS
 fi
+
+for FILE_ZONE in $(find . -name '*.rbl'|cut -b 3-); do
+  grep -wq "$FILE_ZONE" <<< $CUSTOM_ZONES
+  [ $? -ne 0 ] && tmpzone="$tmpzone $RBL_DOMAIN:ip4set:$FILE_ZONE" 
+done
 
 [ -n "$tmpzone" ] && custom_zone="$tmpzone" || custom_zone=""
 
 chown $USERNAME $bl $wl forward
 
 rbldnsd $LOGGING -f -n -r $ZONES -vv -b 0.0.0.0/53 -p $PID_FILE \
-  $RBL_DOMAIN:ip4set:$bl,${wl}$custom_zone \
+  $RBL_DOMAIN:ip4set:$bl,${wl} $custom_zone \
   $RBL_DOMAIN:generic:forward $CUSTOM_CONFIG
